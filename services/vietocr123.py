83359b995c7f728c84700a99b5b9e9ef31eb1bee
CuongEmHuynh <em.huynh@eiu.edu.vn>
2025-12-09 16:59:15 +0700

    #fix for windows

--- services/vietocr123.py
+++ services/vietocr123.py
@@ -9,31 +9,32 @@ from PIL import Image
 import os
 from pdf2image.pdf2image import pdfinfo_from_path
 import gc 
+import sys
 
 def init_vietocr():
     config = Cfg.load_config_from_name('vgg_transformer')
     config['device'] = 'cpu'
     config['cnn']['pretrained'] = True
-    config['predictor']['beamsearch'] = False
+    config['predictor']['beamsearch'] = True
     return Predictor(config)
 
 def init_PaddleOCR():
-    return PaddleOCR(use_doc_orientation_classify=True, lang='vi')
+    return PaddleOCR(use_textline_orientation=False, lang='vi')
 
 vietocr = init_vietocr()
 paddle_detector = init_PaddleOCR()
 
-def pdf_to_images(pdf_input, dpi=300, use_bytes=False):
-    """
-    pdf_input: path (str) hoặc bytes (nếu use_bytes=True)
-    return: list[ PIL.Image ]
-    """
-    poppler_path = "/opt/homebrew/bin"
-    if use_bytes:
-        pages = convert_from_bytes(pdf_input, dpi=dpi,poppler_path=poppler_path)
-    else:
-        pages = convert_from_path(pdf_input, dpi=dpi, poppler_path=poppler_path)
-    return pages
+# def pdf_to_images(pdf_input, dpi=300, use_bytes=False):
+#     """
+#     pdf_input: path (str) hoặc bytes (nếu use_bytes=True)
+#     return: list[ PIL.Image ]
+#     """
+#     poppler_path = "/opt/homebrew/bin"
+#     if use_bytes:
+#         pages = convert_from_bytes(pdf_input, dpi=dpi,poppler_path=poppler_path)
+#     else:
+#         pages = convert_from_path(pdf_input, dpi=dpi, poppler_path=poppler_path)
+#     return pages
 
 
 
@@ -100,6 +101,9 @@ def recognize_boxes_with_vietocr(img_cv, boxes, recognizer: Predictor):
         crop_rgb = cv2.cvtColor(crop, cv2.COLOR_BGR2RGB)
         pil_crop = Image.fromarray(crop_rgb)
 
+        #Save image crop for debug
+        pil_crop.save(f"debug_crop_{x_min}_{y_min}_{x_max}_{y_max}.png")
+
         # Gọi VietOCR
         text = recognizer.predict(pil_crop)
         texts.append(text)
@@ -124,19 +128,39 @@ def iter_pdf_pages(pdf_path, dpi=200, poppler_path=None):
     Generator trả về (page_number, PIL.Image) từng trang một.
     Không bao giờ giữ toàn bộ pages trong RAM cùng lúc.
     """
-    info = pdfinfo_from_path(pdf_path, userpw=None, poppler_path=poppler_path)
-    max_pages = info["Pages"]
 
-    for page_number in range(1, max_pages + 1):
-        images = convert_from_path(
-            pdf_path,
-            dpi=dpi,
-            first_page=page_number,
-            last_page=page_number,
-            poppler_path=poppler_path
-        )
-        # convert_from_path với 1 trang -> images có 1 phần tử
-        yield page_number, images[0]
+    if sys.platform == "win32":
+        print("Running on Windows")
+        info = pdfinfo_from_path(pdf_path, userpw=None)
+        max_pages = info["Pages"]
+        for page_number in range(1, max_pages + 1):
+            images = convert_from_path(
+                pdf_path,
+                dpi=dpi,
+                first_page=page_number,
+                last_page=page_number,
+            )
+            # convert_from_path với 1 trang -> images có 1 phần tử
+            yield page_number, images[0]
+    elif sys.platform == "darwin":
+        info = pdfinfo_from_path(pdf_path, userpw=None, poppler_path=poppler_path)
+        max_pages = info["Pages"]
+
+        for page_number in range(1, max_pages + 1):
+            images = convert_from_path(
+                pdf_path,
+                dpi=dpi,
+                first_page=page_number,
+                last_page=page_number,
+                poppler_path=poppler_path
+            )
+            # convert_from_path với 1 trang -> images có 1 phần tử
+            yield page_number, images[0]
+        print("Running on macOS")
+    else:
+        print(f"Running on another OS: {sys.platform}")
+
+    
 
 def ocr_pdf_paddle_vietocr(
     pdf_input,
@@ -204,7 +228,7 @@ if __name__ == "__main__":
     ocr_pdf_paddle_vietocr(
         pdf_input="./data/file_minio/27135926-0898-12c8-0a0b-3a100476f984_16-20.pdf",
         output_txt="output_paddle_vietocr.txt",
-        dpi=200,
+        dpi=300,
         use_bytes=False,
         device=None  
     )
